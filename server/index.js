325f4d10ee621a484a711d022a97e647969720a9
Mikoafk <mikeycrack71@gmail.com>
2024-07-01 12:08:42 -0500

    Update to deploy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const shirts = require("./shirts.json");
 
+const { join } = require("node:path");
+
+const shirts = require("./shirts.json");
 const PORT = process.env.PORT || 5000;
 
 app.get("/api/shirts", (req, res) => {
@@ -12,5 +14,9 @@ app.post("/api/shirts", (req, res) => {
 	console.log("Publicando nueva camisa...");
 	console.log(req.body);
 });
+
+app.use(express.static(join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => res.sendFile(join(__dirname, "./client/build/index.html")));
 
 app.listen(PORT, () => console.log("Server in PORT:", PORT));
